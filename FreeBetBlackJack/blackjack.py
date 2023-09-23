31a4a3278c5b4c52bbaf2bc4f3215d0281991999
BW <bmweibel@gmail.com>
2023-09-23 01:57:41 -0700

    added betting functionality, basic game routines.

--- FreeBetBlackJack/blackjack.py
+++ FreeBetBlackJack/blackjack.py
@@ -13,18 +13,28 @@ what is needed for classes:
 class BlackJack:
 
     # TODO: Add configs for diff tables - table configs, side bets, etc. 
-    def __init__(self, num_players=1, num_decks=8):
-        self.deck = Deck()
-    
+    def __init__(self, num_players=1, num_decks=8, player_starting_bet=1000):
+        self.deck = Deck(num_decks)
+        self.player_list = [Player(player_starting_bet) for i in range(num_players)]
+
+    def getPlayerList(self):
+        return self.player_list
+
     def play_hand(self, players_list: list[Player]) -> list[bool]:
         dealer = Player(0)
+        # collect player bets
+        bets = [player.getBet() for player in players_list]
         # deal initial hands
         for i in range(2):
             for player in players_list:
                 player.dealCard(self.deck.dealCard())
             dealer.dealCard(self.deck.dealCard())
         # check for BlackJack, offer insurance
-
+        if dealer.getHand()[0] == 'A':  # first card in hand
+            # offer insurance
+            for player in players_list:
+                if player.inHand() and player.wantInsurance():
+                    
         # go thru each player
 
         # dealer plays game
--- FreeBetBlackJack/player.py
+++ FreeBetBlackJack/player.py
@@ -15,18 +15,31 @@ class Player:
         config = configparser.ConfigParser()
         self.money = starting_money
         self.hand = []
+        self.inHand = True
 
-    def setMoney(self, money) -> None:
+    def setMoney(self, money: int) -> None:
         self.money = money
 
     def getMoney(self) -> int:
         return self.money
 
-    def dealCard(self, card) -> None:
+    def dealCard(self, card: str:) -> None:
         self.hand.append(card)
+    
+    def getHand(self) -> list[str]:
+        return self.hand
+    
+    def resetHand(self) -> None:
+        self.hand = []
 
     def isBust(self) -> bool:
         return self.getHandCount() > 21
+
+    def setInHand(self, inHand: bool) -> None:
+        self.inHand = inHand
+    
+    def getInHand(self) -> bool:
+        return self.inHand
 
     def getHandCount(self) -> int:
         count = 0
@@ -45,5 +58,15 @@ class Player:
     # TODO: Implement actual logic of player hand
     def playHand(self, dealer_card: str, curr_value: int, isSoft: bool, isPair: bool) -> str:
         return "St"
+    
+    # TODO: Implement with strategy
+    def wantInsurance(self) -> bool:
+        return False
+    
+    # TODO betting strategy
+    def getBet(self) -> int:
+        bet = 10
+        self.setMoney(self.getMoney() - bet)
+        return bet
 
     
